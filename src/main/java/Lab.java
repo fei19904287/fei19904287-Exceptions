d3dbb6ddcd28c394103dde55608345bf1fd100bb
fei19904287 <feili4287@gmail.com>
2023-02-13 03:47:08 +0000

    throw exceptions

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,3 +1,5 @@
+import java.io.FileInputStream;
+
 public class Lab {
     /**
      * An exception is an unexpected event that occurs during our program.
@@ -20,7 +22,11 @@ public class Lab {
      *
      * Check out this guide on throwing exceptions: https://rollbar.com/guides/java/how-to-throw-exceptions-in-java/#
      */
-    public void mustThrow() {
+    public void mustThrow() throws Exception {
+
+        FileInputStream  fis = new FileInputStream("d//a.txt");
+
+        
 
     }
 }
